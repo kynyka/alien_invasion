dfbde51a29669d67918cfbfc2dffdafd2299f6f4
kynyka <congsu.yeh@gmail.com>
2018-03-27 14:46:43 +0800

    add a ship class & a ship image

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,12 +2,16 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()  # 初始化游戏并创建一个屏幕对象
     ai_settings = Settings()  # 创建实例并存储于变量
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invation')
+
+    # 创建一艘飞船
+    ship = Ship(screen)  # 位置参数
 
     # 开始游戏主循环
     while 1:
@@ -18,6 +22,7 @@ def run_game():
 
         # 每次循环是都重绘屏幕
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
