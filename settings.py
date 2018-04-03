875c941df74b32d15f4b92da4d4a96d74111fda2
kynyka <congsu.yeh@gmail.com>
2018-04-03 12:46:46 +0800

    修改子弹宽度, 生成大子弹, 进一步方便测试 (完毕后改回来)

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings():
 
         # 子弹设置
         self.bullet_speed_factor =1
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60  # 加不加括号一样, 最后bullet_color都是个tuple
         self.bullet_allowed = 3  # 允许同时存在的(即未消失的)子弹数限制为3颗
