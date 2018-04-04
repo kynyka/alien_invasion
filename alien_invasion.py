6f7860c89427816d171f4b57d29517b1b8e591ba
kynyka <congsu.yeh@gmail.com>
2018-04-04 13:48:34 +0800

    点击开始按钮,便能开始游戏

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,7 @@ import game_functions as gf
 def run_game():
     pygame.init()  # 初始化游戏并创建一个屏幕对象
     ai_settings = Settings()  # 创建实例并存储于变量
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))  # 对象screen是一个surface,在Pygame中,surface是屏幕的一部分,用于显示游戏元素.本游戏中,每个元素都是一个surface.现在的它代表整个游戏窗口.
     pygame.display.set_caption('Alien Invation')
 
     # 创建Play按钮
@@ -30,7 +30,7 @@ def run_game():
 
     # 开始游戏主循环
     while 1:
-        gf.check_events(ai_settings, screen, ship, bullets)  # 同时移除本主程序sys模块
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)  # 同时移除本主程序sys模块
 
         if stats.game_active:
             ship.update()
--- game_functions.py
+++ game_functions.py
@@ -33,7 +33,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     '''响应按键和鼠标事件'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,6 +42,15 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    '''在玩家单击Play按钮时开始新游戏'''
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
 
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
@@ -142,9 +151,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     '''响应被外星人撞到的飞船'''
     if stats.ships_left > 0:
         # 将ships_left减1
-        print '-1 Before: '+str(stats.ships_left)
         stats.ships_left -= 1
-        print '-1 After: '+str(stats.ships_left)
 
         # 清空外星人列表和子弹列表
         aliens.empty()
