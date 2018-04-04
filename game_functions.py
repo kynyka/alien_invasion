b72f04bcd625aba9276259736516b42b11432e97
kynyka <congsu.yeh@gmail.com>
2018-04-04 21:38:24 +0800

    修改速度设置

--- game_functions.py
+++ game_functions.py
@@ -104,6 +104,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     if len(aliens) == 0:
         # 删除现有的子弹并新建一群外星人
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
